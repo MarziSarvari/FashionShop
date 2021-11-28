3414a51dffd8edaac0b605b3e37326c7a9ac80b7
Marzi <msarvari2332@gmail.com>
2021-11-28 20:32:48 +0330

    creating productsizes and size controller

---
+++ FashionShop/Controllers/ProductSizeController.cs
@@ -0,0 +1,133 @@
+﻿using AutoMapper;
+using FashionShop.Models;
+using FashionShop.Models.Dtos;
+using FashionShop.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FashionShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductSizeController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ILogger<ProductSizeController> _logger;
+        public ProductSizeController(IUnitOfWork unitOfWork, ILogger<ProductSizeController> logger, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [Authorize]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetProductSizes([FromQuery] RequestParams requestParams)
+        {
+            var productSizes = await _unitOfWork.ProductSizes.GetAll(requestParams, include: q => q.Include(x => x.Size).Include(x => x.Product).ThenInclude(x => x.Color).Include(x => x.Product).ThenInclude(x => x.Style));
+            var result = _mapper.Map<IList<ProductSizeDto>>(productSizes);
+            return Ok(result);
+
+        }
+        [Authorize]
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetProductSize(int id)
+        {
+
+            var productSize = await _unitOfWork.ProductSizes.Get(q => q.Id == id, include: q => q.Include(x => x.Size).Include(x => x.Product).ThenInclude(x => x.Style).Include(x => x.Product).ThenInclude(x=>x.Color).Include(x => x.Product).ThenInclude(x => x.Style).ThenInclude(x => x.MaterialCategory).ThenInclude(i => i.Material).Include(x => x.Product).ThenInclude(x => x.Style).ThenInclude(x => x.MaterialCategory).ThenInclude(i => i.Category).Include(x => x.Product).ThenInclude(x => x.Style).ThenInclude(x => x.Gender));
+            var result = _mapper.Map<ProductSizeDto>(productSize);
+            return Ok(result);
+
+
+        }
+
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateProductSize([FromBody] CreateProductSizeDto ProductSizeDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogInformation($"Invalid Post Attempt in { nameof(CreateProductSize)}");
+                return BadRequest("Invalid Data Inserted");
+            }
+
+            var productSize = _mapper.Map<ProductSize>(ProductSizeDTO);
+            await _unitOfWork.ProductSizes.Insert(productSize);
+            await _unitOfWork.Save();
+
+            return Created("GetProductSize", productSize);
+
+
+        }
+        [Authorize (Roles = "Administrator")]
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateProductSize(int id, [FromBody] UpdateProductSizeDto ProductSizeDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogInformation($"Invalid Update Attempt in { nameof(UpdateProductSize)}");
+                return BadRequest("Invalid Data Inserted");
+            }
+
+            var productSize = await _unitOfWork.ProductSizes.Get(q => q.Id == id);
+            if (productSize == null)
+            {
+                _logger.LogInformation($"Invalid Update Attempt in { nameof(UpdateProductSize)}");
+                return BadRequest("Invalid Data Inserted");
+            }
+            _mapper.Map(ProductSizeDTO, productSize);
+            _unitOfWork.ProductSizes.Update(productSize);
+            await _unitOfWork.Save();
+            return NoContent();
+
+
+        }
+        [Authorize(Roles = "Administrator")]
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> DeleteProductSize(int id)
+        {
+            if (id < 1)
+            {
+                _logger.LogError($"Invalid DELETE attempt in {nameof(DeleteProductSize)}");
+                return BadRequest();
+            }
+
+
+            var productSize = await _unitOfWork.ProductSizes.Get(q => q.Id == id);
+            if (productSize == null)
+            {
+                _logger.LogError($"Invalid DELETE attempt in {nameof(DeleteProductSize)}");
+                return BadRequest("Submitted data is invalid");
+            }
+
+            await _unitOfWork.ProductSizes.Delete(id);
+            await _unitOfWork.Save();
+
+            return NoContent();
+
+        }
+
+    }
+}
---
+++ FashionShop/Controllers/SizeController.cs
@@ -0,0 +1,134 @@
+﻿using AutoMapper;
+using FashionShop.Models;
+using FashionShop.Models.Dtos;
+using FashionShop.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FashionShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SizeController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ILogger<SizeController> _logger;
+        public SizeController(IUnitOfWork unitOfWork, ILogger<SizeController> logger, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [Authorize]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetSizes()
+        {
+          
+            var sizes = await _unitOfWork.Sizes.GetAll(include: q => q.Include(x => x.ProductSizes));
+            var result = _mapper.Map<IList<SizeDto>>(sizes);
+            return Ok(result);
+
+        }
+        [Authorize]
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetSize(int id)
+        {
+
+            var size = await _unitOfWork.Sizes.Get(q => q.Id == id, include: q => q.Include(x => x.ProductSizes));
+            var result = _mapper.Map<SizeDto>(size);
+            return Ok(result);
+
+
+        }
+
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateSize([FromBody] CreateSizeDto SizeDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogInformation($"Invalid Post Attempt in { nameof(CreateSize)}");
+                return BadRequest("Invalid Data Inserted");
+            }
+
+            var size = _mapper.Map<Size>(SizeDTO);
+            await _unitOfWork.Sizes.Insert(size);
+            await _unitOfWork.Save();
+
+            return Created("GetSize", size);
+
+
+        }
+        [Authorize (Roles = "Administrator")]
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateSize(int id, [FromBody] UpdateSizeDto SizeDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogInformation($"Invalid Update Attempt in { nameof(UpdateSize)}");
+                return BadRequest("Invalid Data Inserted");
+            }
+
+            var size = await _unitOfWork.Sizes.Get(q => q.Id == id);
+            if (size == null)
+            {
+                _logger.LogInformation($"Invalid Update Attempt in { nameof(UpdateSize)}");
+                return BadRequest("Invalid Data Inserted");
+            }
+            _mapper.Map(SizeDTO, size);
+            _unitOfWork.Sizes.Update(size);
+            await _unitOfWork.Save();
+            return NoContent();
+
+
+        }
+        [Authorize(Roles = "Administrator")]
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> DeleteSize(int id)
+        {
+            if (id < 1)
+            {
+                _logger.LogError($"Invalid DELETE attempt in {nameof(DeleteSize)}");
+                return BadRequest();
+            }
+
+
+            var size = await _unitOfWork.Sizes.Get(q => q.Id == id);
+            if (size == null)
+            {
+                _logger.LogError($"Invalid DELETE attempt in {nameof(DeleteSize)}");
+                return BadRequest("Submitted data is invalid");
+            }
+
+            await _unitOfWork.Sizes.Delete(id);
+            await _unitOfWork.Save();
+
+            return NoContent();
+
+        }
+
+    }
+}
